0422e352448fdf325c64aeb05857c5b26b39dda8
MiguelGeneroso <gene17051996@gmail.com>
2023-02-02 04:53:58 +0100

    creacion de componentes y assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { EducationPageComponent } from './pages/education-page/education-page.component';
+import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"/home", pathMatch:"full"},
+  {path:"home", component : HomePageComponent},
+  {path:"about", component : AboutPageComponent},
+  {path:"skills", component : SkillsPageComponent},
+  {path:"projects", component : ProjectsPageComponent},
+  {path:"education", component : EducationPageComponent},
+  {path:"about", component : ExperiencePageComponent},
+  {path:"about", component : ContactPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { EducationPageComponent } from './pages/education-page/education-page.component';
+import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    AboutPageComponent,
+    SkillsPageComponent,
+    ProjectsPageComponent,
+    EducationPageComponent,
+    ExperiencePageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
